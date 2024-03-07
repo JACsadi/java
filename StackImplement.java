46d94b39a474a77fef4f813148781bc027b840ed
2021331096 <2021331096@student.sust.edu>
2024-03-07 18:51:49 +0600

    oyshi

--- StackImplement.java
+++ StackImplement.java
@@ -1,63 +1,45 @@
-public class StackImplement {
-    public static class Stack {
-        int[] array= new int[1001];
-        //Stack(int[] i) {this.array = i;};
-
-       int top() {
-             int sz = this.size();
-             if(sz==0){ 
-                System.out.println("Stack underflow");
-                return -1;
-             }
-             return (this.array[sz-1]);
-
-             
+ class Stack{
+    int[] array= new int[1001];
+    int sz = 0;
+    public Stack() {
+        for(int j = 0; j <1001;j++) this.array[j] = -100000000;
+    }
+   int top() {
+         if(sz==0){ 
+            System.out.println("Stack underflow");
+            return -1;
+         }
+         return (this.array[sz-1]);
+    }
+    void pop() {
+        if(sz==0){ 
+            System.out.println("Stack underflow");}
+         else {
+            this.array[sz-1]=-100000000;
+            sz--;
         }
-        void pop() {
-            int sz= this.size();
-            if(sz==0){ 
-                System.out.println("Stack underflow");}
-             else {
-                this.array[sz-1]=0;
-            }
-            }
-
-
-        void push( int j) {
-            int sz = this.size();
-            if(sz==1000){ 
-                System.out.println("Stack overflow");}
-             else {
-                this.array[sz]=j;
-            }
         }
-        int size()
-        {
-            int cnt=0;
-            for( int i=0;i<1001;i++)
-            {
-                if(this.array[i]==0) {
-                    cnt=i;
-                    break;}
-
-            }
-            return (cnt);
-        }
-        
-        boolean empty()
-        {
-            int sz=this.size();
-            if(sz>0){
-                return false;
-            }
-            else { return true;}
-
+    void push( int j) {
+        if(sz==1000){ 
+            System.out.println("Stack overflow");}
+         else {
+            this.array[sz]=j;
+            sz++;
         }
     }
+    int size() {return sz;}
+    boolean empty()
+    {
+        if(sz>0) return false;
+        else  return true;
+    }
+}
 
-    public static void main(String[] args) {
+
+public class StackImplement {
+     public static void main(String[] args) {
                
-        Stack  S = new Stack();
+       Stack  S = new Stack();
        S.push(9);
        S.push(8);
        S.push(12);
